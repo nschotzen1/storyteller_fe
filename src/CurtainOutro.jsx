4119728d0f8c305c50ddbd68b70120dfe0b84c3c
shlomo.chotzen@vimeo.com <shlomo.chotzen@vimeo.com>
2025-04-23 15:16:01 +0300

    ending chat

---
+++ src/CurtainOutro.jsx
@@ -0,0 +1,14 @@
+const CurtainOutro = () => {
+    return (
+      <div className="curtain-outro fixed inset-0 bg-black flex items-end justify-center z-50 animate-slideDown">
+        <img
+          src="/tapestries/curtain.png"
+          alt="Curtain"
+          className="w-full max-h-full object-cover"
+        />
+      </div>
+    );
+  };
+  
+  export default CurtainOutro;
+  
